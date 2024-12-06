48d00abf5d0950d05e228a63a67e9563914cc054
Anurag-Mishra22 <anurag2025mishra@gmail.com>
2024-12-06 12:49:23 +0530

    build the routes,handlers and email templates

--- mail-service/cmd/api/routes.go
+++ mail-service/cmd/api/routes.go
@@ -4,13 +4,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 )
 
 func (app *Config) routes() http.Handler {
 	mux := chi.NewRouter()
 
-	// specify who is allowed to connect (Middlerware setup)
+	// specify who is allowed to connect
 	mux.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
@@ -20,6 +21,9 @@ func (app *Config) routes() http.Handler {
 		MaxAge:           300,
 	}))
 
-	return mux
+	mux.Use(middleware.Heartbeat("/ping"))
 
+	mux.Post("/send", app.SendMail)
+
+	return mux
 }
